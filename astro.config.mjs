e9ed27b62b3c44844b2cd58f032a45f8e2ea90f0
Nathan Van Bignoot <contact@nathanvanbignoot.fr>
2025-04-13 21:59:03 +0200

    feat: add support for node server + add support for docker

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,18 @@ import { defineConfig } from "astro/config";
 
 import vue from "@astrojs/vue";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [vue()],
+
   devToolbar: {
     enabled: true,
   },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
